92bc29fdbd0498c5481d952506dda9183165a4c3
Romantic <m15115021148@163.com>
2017-03-02 09:24:40 +0800

    测试一次

--- app/src/main/java/com/geek/popwindowdemo/TestActivity.java
+++ app/src/main/java/com/geek/popwindowdemo/TestActivity.java
@@ -2,9 +2,11 @@ package com.geek.popwindowdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.security.MessageDigest;
 import java.util.Arrays;
 
 public class TestActivity extends AppCompatActivity implements CustomPopView.OnItemListener{
@@ -18,6 +20,7 @@ public class TestActivity extends AppCompatActivity implements CustomPopView.OnI
         setContentView(R.layout.activity_test);
         mContext = this;
         popView = new CustomPopView(this, Arrays.asList(key),this);
+        Log.e("result",MD5("123456"));
     }
 
     public void onShow(View view) {
@@ -46,5 +49,38 @@ public class TestActivity extends AppCompatActivity implements CustomPopView.OnI
 
     public void onRight(View view) {
         popView.showRight();
+    }
+
+    /**
+     * MD5加密，32位
+     * @param str
+     * @return
+     */
+    public static String MD5(String str) {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        char[] charArray = str.toCharArray();
+        byte[] byteArray = new byte[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++) {
+            byteArray[i] = (byte) charArray[i];
+        }
+        byte[] md5Bytes = md5.digest(byteArray);
+
+        StringBuffer hexValue = new StringBuffer();
+        for (int i = 0; i < md5Bytes.length; i++) {
+            int val = ((int) md5Bytes[i]) & 0xff;
+            if (val < 16) {
+                hexValue.append("0");
+            }
+            hexValue.append(Integer.toHexString(val));
+        }
+        return hexValue.toString().toUpperCase();
     }
 }
